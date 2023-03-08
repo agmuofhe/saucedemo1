3add4305d8e0e37925cfdce3d311fc0d13a77f12
agmuofhe <98636959+agmuofhe@users.noreply.github.com>
2023-03-08 23:43:48 +0200

    update

--- src/test/java/stepDef/addToCartStepDef.java
+++ src/test/java/stepDef/addToCartStepDef.java
@@ -33,6 +33,7 @@ public class addToCartStepDef {
             System.out.println("mac");
         } else if (osName.contains("linux")) {
             System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/linux/chromedriver");
+            options.setPlatformName("LINUX");
             System.out.println("LINUX");
         }
 
