2b95b7a7f0bd218359b255e70386dcd6ea769429
agmuofhe <98636959+agmuofhe@users.noreply.github.com>
2023-03-10 08:17:41 +0200

    Update addToCartStepDef.java

--- src/test/java/stepDef/addToCartStepDef.java
+++ src/test/java/stepDef/addToCartStepDef.java
@@ -46,8 +46,8 @@ public class addToCartStepDef {
         
 //         options.addArguments("--disable-blink-features");
 //         options.addArguments("--disable-blink-features=AutomationControlled");
-        options.addArguments("--headless");
-        options.addArguments("--disable-gpu");
+//         options.addArguments("--headless");
+//         options.addArguments("--disable-gpu");
 //         options.addArguments("--disable-extensions");
 //         options.addArguments("--incognito");
 //         options.addArguments("--disable-plugins-discovery");
