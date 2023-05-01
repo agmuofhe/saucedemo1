d1fe5b739366966a7fb7382d81a37165c7052d6c
agmuofhe <98636959+agmuofhe@users.noreply.github.com>
2023-05-01 11:26:00 +0200

    Update AddToCartTest.java

--- src/test/java/runner/AddToCartTest.java
+++ src/test/java/runner/AddToCartTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/java/FeatureFile/addToCart.feature",
         glue = {"stepDef"},
-        plugin = {"pretty", "html:target/report.html"}
+        plugin = {"pretty", "html:target/report.json"}
 )
 
 public class AddToCartTest {
